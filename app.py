4d20c567b21e2d4fb49186545c15cd41b7430eec
mayurrkukreja <mayurkukreja02@gmail.com>
2022-06-23 11:04:04 +0530

    modify app.py

--- app.py
+++ app.py
@@ -1,33 +1,20 @@
-import openpyxl
 from pandasgui import show
 
 import pandas as pd
 
 # Read Excel Sheet
-file = 'test.xlsx'
+file = 'details.xlsx'
 # Get Data
 data = pd.ExcelFile(file)
 
-# Outputs Excel Sheet Names
+# Output Excel Sheet Names
 print(data.sheet_names)
 # Parse Data into Pandas Library
-df = data.parse('Sheet1')
+df = data.parse('details')
 
 # Outputs all DataFields
 print(df.head())
 
-output = df[['Name','RollNo','Address']]
+output = df[['CVE','Risk','Host','Synopsis','Description','Solution', 'Risk Factor']]
+ 
 show(output)
-
-
-# Testing Code Below && Do not UnComment below code
-
-# ps = openpyxl.load_workbook('test.xlsx')
-# sheet = ps['Sheet1']
-
-# print(ps['Name'].iloc[0])
-
-# workbook = pd.read_excel('test.xlsx')
-
-# # sheet = workbook['Sheet1']
-# # print(workbook['Name'].iloc[0]) 
